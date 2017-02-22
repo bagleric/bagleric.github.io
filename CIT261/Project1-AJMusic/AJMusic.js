c3174e4e564bc2f154af38b5d0a43c9a51bbf07c
Eric <bag12008@byui.edu>
2017-02-21 22:15:04 -0700

    update

--- CIT261/Project1-AJMusic/AJMusic.js
+++ CIT261/Project1-AJMusic/AJMusic.js
@@ -1,29 +1,20 @@
-function showTip(myEvent)
+function getTips()
 {
+    console.log("Function: getTips");
             xmlhttp = new XMLHttpRequest();
             xmlhttp.onreadystatechange = function () {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    alert("we're number 4");
-                    document.getElementById('jsonFile').innerHTML = xmlhttp.responseText;
+                 
+                  console.log("AJAX: succesful");  
+                    var object = JSON.parse(xmlhttp.responseText);
+                    console.log(object);
+                    
+                    sessionStorage.setItem("tips", object);
                 }
-                else if (xmlhttp.readyState == 3) {
-                    document.getElementById('jsonFile').innerHTML = "we're number 3";
-                    alert("we're number 3");
-                }
-                else if (xmlhttp.readyState == 2) {
-                    document.getElementById('jsonFile').innerHTML = "we're number 2";
-                    alert("we're number 2");
-                }
-                else if (xmlhttp.readyState == 1) {
-                    document.getElementById('jsonFile').innerHTML = "we're number 1";
-                    alert("we're number 1");
-                }
-                else {
-                    document.getElementById('jsonFile').innerHTML = "Waiting for server Response ...";
+                else{
+                    console.log("AJAX: working");
                 }
             }
             xmlhttp.open("GET", "https://bagleric.github.io/CIT261/Project1-AJMusic/tasks.json", true);
             xmlhttp.send();
         }
-
-}

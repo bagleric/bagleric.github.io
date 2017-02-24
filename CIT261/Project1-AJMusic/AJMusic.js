b0539dfe0dbd4f4674209beba8847b5b76308ae7
bagleric <bag12008@byui.edu>
2017-02-24 13:35:20 -0700

    update

--- CIT261/Project1-AJMusic/AJMusic.js
+++ CIT261/Project1-AJMusic/AJMusic.js
@@ -1,8 +1,8 @@
 function loadTasks() {
+    console.log("Function: loadTasks");
     if (sessionStorage.getItem("tasks") !== undefined) {
         return;
     }
-    console.log("Function: getTips");
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
